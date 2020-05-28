301fb1fe4e85ae29be65e8e7056b20e628f04fc6
js_krishna <jskreation@gmail.com>
2020-05-28 22:02:13 +0530

    worked on sidebar and table component in admin panel.

--- src/app/admin/components/table-policy/table-policy.component.ts
+++ src/app/admin/components/table-policy/table-policy.component.ts
@@ -1,47 +1,79 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { Title } from '@angular/platform-browser';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
+import { Title } from "@angular/platform-browser";
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  date: string;
+  renewDate: string;
+  policy: string;
+  policyHolder: string;
+  insurance: string;
+  payment: string;
+  renew: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  {
+    date: "12/02/2020",
+    renewDate: "11/03/2021",
+    policy: "#00001",
+    policyHolder: "Bob Brown",
+    payment: "Car",
+    insurance: "Monthly",
+    renew: "Apply to Renew ",
+  },
+  {
+    date: "12/02/2020",
+    renewDate: "11/03/2021",
+    policy: "#00001",
+    policyHolder: "Bob Brown",
+    payment: "Car",
+    insurance: "Monthly",
+    renew: "Apply to Renew ",
+  },
+  {
+    date: "12/02/2020",
+    renewDate: "11/03/2021",
+    policy: "#00001",
+    policyHolder: "Bob Brown",
+    payment: "Car",
+    insurance: "Monthly",
+    renew: "Apply to Renew ",
+  },
+  {
+    date: "12/02/2020",
+    renewDate: "11/03/2021",
+    policy: "#00001",
+    policyHolder: "Bob Brown",
+    payment: "Car",
+    insurance: "Monthly",
+    renew: "Apply to Renew ",
+  },
 ];
 
 @Component({
-  selector: 'app-table-policy',
-  templateUrl: './table-policy.component.html',
-  styleUrls: ['./table-policy.component.scss']
+  selector: "app-table-policy",
+  templateUrl: "./table-policy.component.html",
+  styleUrls: ["./table-policy.component.scss"],
 })
 export class TablePolicyComponent implements OnInit {
-
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = [
+    "date",
+    "renewDate",
+    "policy",
+    "policyHolder",
+    "insurance",
+    "payment",
+    "renew",
+  ];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  constructor(
-    private titleService: Title
-  ) { }
+  constructor(private titleService: Title) {}
 
   ngOnInit() {
-    this.titleService.setTitle('angular-material-template - Customers');
     this.dataSource.sort = this.sort;
-
   }
 }
