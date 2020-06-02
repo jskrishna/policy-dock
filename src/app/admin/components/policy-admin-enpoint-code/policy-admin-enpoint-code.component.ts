a3e8f503baafcaa493807dd7b3727ce21820dfc7
js_krishna <jskreation@gmail.com>
2020-06-03 01:22:01 +0530

    worked on front end component

--- src/app/admin/components/policy-admin-enpoint-code/policy-admin-enpoint-code.component.ts
+++ src/app/admin/components/policy-admin-enpoint-code/policy-admin-enpoint-code.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-policy-admin-enpoint-code',
-  templateUrl: './policy-admin-enpoint-code.component.html',
-  styleUrls: ['./policy-admin-enpoint-code.component.scss']
+  selector: "app-policy-admin-enpoint-code",
+  templateUrl: "./policy-admin-enpoint-code.component.html",
+  styleUrls: ["./policy-admin-enpoint-code.component.scss"],
 })
 export class PolicyAdminEnpointCodeComponent implements OnInit {
-
   public selectedValue: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
